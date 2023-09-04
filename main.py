3969fd3162e0314f41bad057773f6f2a5d004edb
Siddharth <vengavasi122@outlook.com>
2023-09-03 22:24:32 -0500

    I think it is finally finished.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import random
 import os
 import platform
+import time
+
+
+def again():
+    print("Do you want to play again?")
+    go_or_no = input('Y/N: ')
+    if go_or_no == 'Y' or go_or_no == 'y':
+        print("Running...")
+        time.sleep(2)
+        clear()
+        go()
+    elif go_or_no == 'N' or go_or_no == 'n':
+        print("Stopping...")
+        time.sleep(2)
+        clear()
+        return 0
 
 
 def clear():
@@ -11,60 +27,69 @@ def clear():
 
 
 print("Welcome to hangman!")
-print("You will have ten attempts")
-
+time.sleep(2)
+print("You will have ten attempts.")
+time.sleep(2)
 
 def load_words():
     with open('words_alpha.txt') as word_file:
         valid_words = set(word_file.read().split())
 
     return valid_words
-stop_at = 6
-wrong = 0
-g = load_words()
-a = list(g)
-b = random.choice(a)
-c = len(b)
-d = "-" * c
-print(d)
-word = list(d)
-gues = ""
-while True:
-
-    def get_single_letter_input():
-        while True:
-            gues = input("Enter a letter: ")
-            if len(gues) == 1:
-                return gues
 
 
-    word_so_far = "".join("-" for letter in b)
-    for n in range(10):
-        guess = get_single_letter_input()
-        if guess in b:
-            word_so_far = "".join(x if x in guess else word_so_far[i]
-                                  for i, x in enumerate(b))
+def go():
+    stop_at = 10
+    wrong = 0
+    g = load_words()
+    a = list(g)
+    b = random.choice(a)
+    c = len(b)
+    d = "-" * c
+    print(d)
+    word = list(d)
+    tries = 0
+    while True:
 
-            word_so_far = list(word_so_far)
-            wor = word_so_far.index(guess)
-            word_so_far[wor] = guess
-            word = ''.join(word_so_far)
-            print(f"{word}")
-            print(f"Wrong: {wrong}")
-        else:
-            if wrong == stop_at:
-                print("All over.")
-                break
+        def get_single_letter_input():
+            while True:
+                gues = input("Enter a letter: ")
+                if len(gues) == 1:
+                    return gues
+
+        word_so_far = "".join("-" for letter in b)
+        for n in range(10):
+            guess = get_single_letter_input()
+            if guess in b:
+                word_so_far = "".join(x if x in guess else word_so_far[i]
+                                      for i, x in enumerate(b))
+
+                word_so_far = list(word_so_far)
+                wor = word_so_far.index(guess)
+                word_so_far[wor] = guess
+                word = ''.join(word_so_far)
+                print(f"{word}")
+                print(f"Wrong: {wrong}")
+                if '-' in word:
+                    break
+                else:
+                    clear()
+                    print("Finished!")
+                    time.sleep(2)
+                    print(f"The word was, {word}")
+                    time.sleep(2)
+                    print(f"You got it in {tries} tries with {wrong} mistakes.")
+                    time.sleep(2)
+                    again()
+                tries += 1
             else:
-                wrong = wrong + 1
-            word_so_far = "".join(x if x in guess else word_so_far[i]
-                                  for i, x in enumerate(b))
-
-            word_so_far = list(word_so_far)
-            wor = word_so_far.index(guess)
-            word_so_far[wor] = guess
-            word = ''.join(word_so_far)
-            print("Wrong!")
-            print(f"{word}")
-            print(f"Wrong: {wrong}")
-
+                if wrong == stop_at:
+                    print("All over.")
+                    clear()
+                else:
+                    wrong = wrong + 1
+                print("Wrong!")
+                print(f"{word}")
+                print(f"Wrong: {wrong}")
+                tries += 1
+go()
